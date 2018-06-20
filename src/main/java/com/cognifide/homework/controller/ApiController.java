caf809b4b7d3254834751060148c1d304b787a58
Michał Kowalczewski <michal.kowalczewski@atos.net>
2018-06-21 00:33:57 +0200

    added Book by category functionality

--- src/main/java/com/cognifide/homework/controller/ApiController.java
+++ src/main/java/com/cognifide/homework/controller/ApiController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ApiController {
@@ -24,6 +27,21 @@ public class ApiController {
             return jsonParser.getBooksMap().get(isbn);
         else
             throw new ResourceNotFoundException();
+    }
+    
+    @GetMapping("/api/category/{category}/books")
+    public List<Book> findByCategory(@PathVariable("category") String category){
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Book> books = new ArrayList<>();
+        for (Map.Entry<String, Book> entry: jsonParser.getBooksMap().entrySet()) {
+            if (entry.getValue().getCategories() != null) {
+                for (int i = 0; i < entry.getValue().getCategories().length; i++) {
+                    if (entry.getValue().getCategories()[i].equals(category))
+                        books.add(entry.getValue());
+                }
+            }
+        }
+        return books;
     }
 
 }
